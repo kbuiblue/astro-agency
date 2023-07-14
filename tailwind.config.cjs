2ce83940b66ad2dfa23979141bf077c48dd67134
Khanh Bui <kbuiblue@gmail.com>
2023-07-14 14:34:20 +0700

    Changed title and background elements

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	mode: "jit",
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
 		extend: {
+			backgroundImage: {
+				"hero-pattern": "url('/layered-waves-haikei.svg')",
+			},
 			colors: {
 				body: "rgb(var(--color-bg))",
 				"box-bg": "rgb(var(--color-box))",
 				"box-shadow": "rgb(var(--box-sd))",
 				"box-border": "rgb(var(--box-border))",
-				primary: "#1d4ed8",
+				primary: "#a47cbe",
 				"heading-1": "rgb(var(--heading-1))",
 				"heading-2": "rgb(var(--heading-2))",
 				"heading-3": "rgb(var(--heading-3))",
